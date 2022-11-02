2a5cc12709634a832bc669b4e622a197c27acbe0
tor <torfrancis447@gmail.com>
2022-11-01 20:37:59 -0400

    commit tree 1e03797a47889d82f86259d7a7ad49943a5146ec

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+// function razzle(lawyer = "Billy", target ="'em") {
+//     console.log(`${lawyer} razzle-dazzle ${target}!`);
+// }
+
+
+function saturdayFun(target="roller-skate") {
+    return (`This Saturday, I want to ${target}!`)    
+}
+
+// const fn = function () {
+//     console.log("Yet more razzling");
+//   };
+
+function mondayWork(response="go to the office") {
+    return (`This Monday, I will ${response}.`)
+}
+
+function wrapAdjective(vf = '*') {
+    // function(vf = '*') {
+    return function(adjective = 'special') {
+        return(`You are ${vf}${adjective}${vf}!`)
+        
+        };
+    // };
+}
